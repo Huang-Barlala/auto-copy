6e8ebc59aa417f150e2e39708c723323d2130fdf
Barlala <1174418120@163.com>
2025-06-15 18:45:42 +0800

    ✨ feat(tauri): add single instance plugin
    - 使用 single instance plugin 保证应用单例运行
    - 添加自定义图标

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -125,6 +125,12 @@ async fn start_watching_path(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(
             tauri_plugin_log::Builder::new()
                 .targets([
